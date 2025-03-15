1e8aeeb3bc78873e36f7aacb46ac33c268db05ec
Gonzalo <gonzalohm93@gmail.com>
2025-03-15 21:48:57 +0100

    fix model user

--- api/models/User.js
+++ api/models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema(
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    avatar: { type: String, required: true },
+    avatar: { type: String },
     vip: { type: Boolean, default: false }
   },
   { timestamps: true, collection: 'users' }
